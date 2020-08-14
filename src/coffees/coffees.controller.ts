52c8596fd2e7ec1928ab285603bf0a30fd1354f5
Amber Cyr <seylovie@gmail.com>
2020-08-13 21:17:45 -0700

    added placeholder post patch and delete routes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll(){
-        return "returns all coffees"
-    }
+  @Get('flavors') // route: GET coffees/flavors
+  findAll(): string {
+    return 'returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): string {
+    return `returns a coffee with id: ${id}`;
+  }
+
+  @Post()
+  // @HttpCode(HttpStatus.GONE) set specific http status on a route. In Postman, this will show up under the 'status' property. because of this it will always return 410: GONE
+  create(@Body() body: string): string {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() body): string {
+    return `updated coffee at id: ${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return `deleted coffee at id: ${id}`;
+  }
 }
