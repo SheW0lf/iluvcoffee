d3369593e3a7d572394c2074fa5be13bf6d980d1
Amber Cyr <ambercyr@acyr-mbp.local>
2020-09-15 12:58:55 -0700

    updated the port number

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [CoffeesModule, TypeOrmModule.forRoot({
     type: 'postgres', // type of database
     host: 'localhost', // host name. Localhost since this is in local dev
-    port: 5432, // external port database is running on. see docker-compose.yml
+    port: 6432, // external port database is running on. see docker-compose.yml
     username: 'postgres', // default username for postgres databases. can also set one in the docker-compose file
     password: 'pass123', // password. see docker-compose.yml
-    database: 'test', // set database name
+    database: 'postgres', // default database name. If db name not specified in docker-compose.yml, this name is the same as the username specified above
     autoLoadEntities: true, // loads modules automatically without specifying the entities array
     synchronize: true // ensures the TypeORM entities are synchronized with the database. This needs to be disabled in prod
   })],
