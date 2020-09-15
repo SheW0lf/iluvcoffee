75ed56bcfe3b21c2b478382c39e29f8cad0b25e9
Amber Cyr <ambercyr@acyr-mbp.local>
2020-09-15 14:51:59 -0700

    added TypeORM decorators

--- src/coffees/entities/coffees.entity.ts
+++ src/coffees/entities/coffees.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity() // generates a sql table named 'coffee' based on the classname. if you prefer a different name other than the class name write it in the Entity parentheses
 export class Coffee {
+    @PrimaryGeneratedColumn() // indicates we want this value to be automatically incremented by the database
     id: number;
+
+    @Column()
     brand: string;
+
+    @Column('json', {nullable: true}) // indicates that the flavors should be stored as json and that they are optional with the nullable property
     flavors: string[];
 }
