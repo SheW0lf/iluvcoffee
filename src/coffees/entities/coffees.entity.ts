1fc35914a228560091f57bd16a9db452a3c959e2
Amber Cyr <ambercyr@acyr-mbp.local>
2020-09-15 16:34:16 -0700

    added cascade and Flavor type to flavors prop

--- src/coffees/entities/coffees.entity.ts
+++ src/coffees/entities/coffees.entity.ts
@@ -10,6 +10,12 @@ export class Coffee {
     brand: string;
 
     @JoinTable()
-    @ManyToMany(type => Flavor, flavor => flavor.coffees)
-    flavors: string[];
+    @ManyToMany(
+        type => Flavor, 
+        flavor => flavor.coffees,
+        {
+            cascade: true
+        }
+    )
+    flavors: Flavor[];
 }
