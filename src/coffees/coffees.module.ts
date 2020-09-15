2e4156236b3f65653df19dc3a6fdadf85d2fcf07
Amber Cyr <ambercyr@acyr-mbp.local>
2020-09-15 14:42:49 -0700

    imported coffee entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffees.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({ 
+    imports: [TypeOrmModule.forFeature([Coffee])], // adds the coffee entity to the coffee module for use
+    controllers: [CoffeesController], 
+    providers: [CoffeesService] 
+}) 
 export class CoffeesModule {}
