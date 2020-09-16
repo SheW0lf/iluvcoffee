6b37ad6f247bb168eec5192d6fb5a14b55169cbd
Amber Cyr <ambercyr@acyr-mbp.local>
2020-09-15 17:01:32 -0700

    added implicit conversion transform option

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
             whitelist: true, //whitelist strips all other properties not defined in the DTO from the req (incase any were added maliciously or otherwise) so they do not get added to the res obj
             forbidNonWhitelisted: true, // this will throw an error if any other properties not defined in the DTO were added to the req
             transform: true, // transforms variables to instances of the data they are described to be. example id comes from @Param as a string but if we type it as a number, transform will do the conversion without a pipe
+            transformOptions: {
+                enableImplicitConversion: true // no longer need to use the Type decorator to indicate the prop types, instead they will be assigned by the TS type definitions
+            }
         }),
     );
     await app.listen(3000);
