570a2410deac5fcb57090e03524e603fba945e24
Amber Cyr <ambercyr@acyr-mbp.local>
2020-09-15 17:03:55 -0700

    added pagonation dto

---
+++ src/common/dto/pagination-query.dto.ts
@@ -0,0 +1,12 @@
+import { IsOptional, IsPositive } from "class-validator";
+import { Type } from 'class-transformer'
+
+
+export class PaginationQueryDto {
+    @IsOptional()
+    @IsPositive()
+    limit: number;
+
+    @IsOptional()
+    offset: number
+}
